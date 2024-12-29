af3b0c8b62ec2cb8b69b50667ffc74c034ffce2d
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 18:30:53 -0300

    Realizando actividad 3-4 aciertos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-//Te recomendamos realizar el siguiente desafío en Visual Studio Code:<br>
-//1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si.<br>
-//A. Cometiste un delito
-//B. Eres inocente
-//En caso de estar haber cometido un delito debe devolver por consola un mensaje que diga "Iras a la cárcel"
-//En caso de ser inocente debe devolver por consola un mensaje que diga "Puedes ir a casa"
-let pares = 0;
-let impares = 0;    
+//Te recomendamos realizar el siguiente desafío en Visual Studio Code:
+//1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si te alcanza para comprar figuritas del mundial.
+//A. Si tienes menos de $20 ó $20 debe devolver por consola un mensaje que diga "Te puedo ofrecer 2 caramelos"
+//B. Si tienes entre de $21 y $49 debe devolver por consola un mensaje que diga "Estas cerca, pero no puedo fiarte, vuelve mañana."
+//C. Si tienes $50 o más debe devolver por consola un mensaje que diga "Aquí tienes tus figuritas del mundial"
+
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-    let ingreso = prompt('El resultado de la investigación fue que:');
-        if (ingreso == "Cometiste un delito") {
-            console.log("Irás a la cárcel");
-        } else if (ingreso == "Eres inocente"){
-            console.log("Puedes ir a casa");
+    let dinero = parseFloat(prompt('¿Cuánto dinero tenés? Ingresar valor en $'));
+        if (dinero <= 20) {
+            console.log("Te puedo ofrecer 2 caramelos");
+        } else if (dinero <= 49){
+            console.log("Estas cerca, pero no puedo fiarte, vuelve mañana.");
+        } else {
+            console.log("Aquí tienes tus figuritas del mundial");
         }
     })
-//le cuesta detectar los valores inicializados dentro de los condicionales 
+ 
