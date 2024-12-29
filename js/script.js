54bf956530bcfdd897450f158bde7c4429685e04
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 16:53:09 -0300

    Realizando actividad 01

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
-let cadena ="";
-let valor;
+
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-    valor = prompt("Ingrese valor a sumar");
-    while (valor != "ESC") {
-        cadena += valor;
-        console.log(cadena);
-        valor = prompt("Ingrese valor a sumar");
-    } 
-})
+//Actividad 01:
+let cantidad = prompt('INGRESE CANTIDAD DE REPETICIONES');
+let texto = prompt('INGRESE TEXTO A REPETIR');
+for (let index = 0; index < cantidad; index++) {
+console.log(texto);
+}})
+
+//¿Qué tiene que hacer este código? Repetir un texto ingresado, la cantidad de veces que se requiere.
+//¿Por qué no cumple con su función? Porque nunca se actualizaba la variable index, siempre era 0 por lo cual se repetía infinitamente ya que la condción siempre iba a ser verdadera
