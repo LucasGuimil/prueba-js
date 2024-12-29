ba2236572f55c9df6cfec9da36464100048e7abc
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 17:14:59 -0300

    Realizando actividad 2

--- js/script.js
+++ js/script.js
@@ -2,12 +2,19 @@
 
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-//Actividad 01:
-let cantidad = prompt('INGRESE CANTIDAD DE REPETICIONES');
-let texto = prompt('INGRESE TEXTO A REPETIR');
-for (let index = 0; index < cantidad; index++) {
-console.log(texto);
-}})
+//Actividad 02:
+let lados = prompt('INGRESE CANTIDAD DE LADOS');
+for (let index = 0; index < lados; index++) {
+if (lados < 3) {
+    alert("Faltan lados");
+    break;
+} else {
+    let cuenta = index + 1;
+    alert("lado" + cuenta);
+}
+}
+})
 
-//¿Qué tiene que hacer este código? Repetir un texto ingresado, la cantidad de veces que se requiere.
-//¿Por qué no cumple con su función? Porque nunca se actualizaba la variable index, siempre era 0 por lo cual se repetía infinitamente ya que la condción siempre iba a ser verdadera
+//¿Qué tiene que hacer este código? Parecería que hay que ingresar la cantidad de lados de un polígono y verifica si la cantidad de lados es correcta.
+//¿Por qué no cumple con su función? Porque la alerta está fuera del condicional.
+//¿Qué propuesta podrías hacer para que tenga sentido su uso? Agregar más detalles a la alerta y ubicarla en el condicional.
