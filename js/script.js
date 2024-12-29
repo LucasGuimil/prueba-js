0661c2034cbb1ef728f86071a46809a271a3f6b1
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 18:15:55 -0300

    Realizando desafio de 0 aciertos

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
-//Actividad 02:
-//Crea un algoritmo que le pida al usuario un número y luego utiliza while para evaluar cuántos números son pares y cuantos impares, hasta que el usuario ingrese "salir".  
+//Te recomendamos realizar el siguiente desafío en Visual Studio Code:<br>
+//1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si.<br>
+//A. Esta lloviendo<br>
+//B. Esta soleado<br>
+//En caso de estar lloviendo debe devolver por consola un mensaje que diga "Lleva paraguas"<br>
+//En caso de estar soleado debe devolver por consola un mensaje que diga "No olvides tus shorts"</p>
+
 let pares = 0;
 let impares = 0;    
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-    let ingreso = prompt('Ingrese un número');
-    while (ingreso !== "salir") {
-        ingreso = parseFloat(ingreso)
-        if (ingreso % 2 === 0) {
-            pares++;
-            console.log(ingreso);
-        } else {
-            impares++; 
-            console.log(ingreso);
+    let ingreso = prompt('¿Cómo está el clima hoy');
+        if (ingreso == "Esta lloviendo") {
+            console.log("Lleva paraguas");
+        } else if (ingreso == "Esta soleado"){
+            console.log("No olvides tus shorts");
         }
-        ingreso = prompt('Ingrese un número')
-        }
-    console.log('Cantidad total de números pares: ' + pares);
-    console.log('Cantidad total de números impares: ' + impares);
     })
 //le cuesta detectar los valores inicializados dentro de los condicionales 
