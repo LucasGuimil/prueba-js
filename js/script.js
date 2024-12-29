c91151593518d3c6189bdad4afe0d256452ba00c
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 17:22:09 -0300

    Realizando actividad 1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
-//Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
+//Actividad 01:
+//Crea un algoritmo que le pida al usuario que ingrese un número y luego evalúe si ese número es par o impar.
 
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-//Actividad 02:
-let lados = prompt('INGRESE CANTIDAD DE LADOS');
-for (let index = 0; index < lados; index++) {
-if (lados < 3) {
-    alert("Faltan lados");
-    break;
-} else {
-    let cuenta = index + 1;
-    alert("lado" + cuenta);
-}
-}
-})
-
-//¿Qué tiene que hacer este código? Parecería que hay que ingresar la cantidad de lados de un polígono y verifica si la cantidad de lados es correcta.
-//¿Por qué no cumple con su función? Porque la alerta está fuera del condicional.
-//¿Qué propuesta podrías hacer para que tenga sentido su uso? Agregar más detalles a la alerta y ubicarla en el condicional.
+    let ingreso = prompt('Ingrese un número');
+    if (ingreso % 2 === 0) {
+        alert("Es un número par");
+    } else {
+        alert("Es un número impar")
+    }
+    })
