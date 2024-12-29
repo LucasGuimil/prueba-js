fb236c6206995b3a9f74ff83bbdcdbf24e7309cd
LucasGuimil <l.d.guimil@gmail.com>
2024-12-29 18:23:38 -0300

    Realizando actividad 1-2 aciertos

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 //Te recomendamos realizar el siguiente desafío en Visual Studio Code:<br>
 //1. Crea un algoritmo que a partir de la respuesta del usuario pueda determinar si.<br>
-//A. Esta lloviendo<br>
-//B. Esta soleado<br>
-//En caso de estar lloviendo debe devolver por consola un mensaje que diga "Lleva paraguas"<br>
-//En caso de estar soleado debe devolver por consola un mensaje que diga "No olvides tus shorts"</p>
-
+//A. Cometiste un delito
+//B. Eres inocente
+//En caso de estar haber cometido un delito debe devolver por consola un mensaje que diga "Iras a la cárcel"
+//En caso de ser inocente debe devolver por consola un mensaje que diga "Puedes ir a casa"
 let pares = 0;
 let impares = 0;    
 const boton = document.getElementById("boton");
 boton.addEventListener('click',() => {
-    let ingreso = prompt('¿Cómo está el clima hoy');
-        if (ingreso == "Esta lloviendo") {
-            console.log("Lleva paraguas");
-        } else if (ingreso == "Esta soleado"){
-            console.log("No olvides tus shorts");
+    let ingreso = prompt('El resultado de la investigación fue que:');
+        if (ingreso == "Cometiste un delito") {
+            console.log("Irás a la cárcel");
+        } else if (ingreso == "Eres inocente"){
+            console.log("Puedes ir a casa");
         }
     })
 //le cuesta detectar los valores inicializados dentro de los condicionales 
