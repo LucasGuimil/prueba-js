c6c8ccdaf7ee47cec9e399546942887ed123c19c
LucasGuimil <l.d.guimil@gmail.com>
2025-01-01 17:22:52 -0300

    Realizando prácticas de la parte teórica

--- js/script.js
+++ js/script.js
@@ -1,11 +1,55 @@
-//Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
+// Declarar la función sin parámetros 
 
-let numeroInicial = prompt("Ingrese el primer valor");
-numeroInicial = Number(numeroInicial)
-for (let i = 0; i <10 ; i++) {
-    let numeroSuma = prompt("Ingrese valor a sumar");
-    numeroSuma = Number(numeroSuma)
-    suma = numeroInicial + numeroSuma;
-    console.log(suma);
-    numeroInicial = suma;
+function mostrarLibros() {
+    console.log("Bienvenido a la biblioteca. ¡Estos son los libros disponibles!");
 }
+
+mostrarLibros();
+
+function buscarLibro(titulo) {
+    console.log(`Buscando el libro: ${titulo}...`);
+}
+
+buscarLibro("El principito");
+
+function calcularMulta(diasRetraso) {
+    const multaPorDia = 0.50;
+    return diasRetraso * multaPorDia;
+}
+
+let multa = calcularMulta(5);
+console.log(`La multa total es: $${multa}`);
+
+function agregarLibro(titulo, autor) {
+    return {titulo, autor};
+}
+
+function mostrarDetalleLibro(libro) {
+    console.log(`Título: ${libro.titulo}, Autor: ${libro.autor}`);
+}
+
+let libro1 = agregarLibro("1984", "George Orwell");
+
+mostrarDetalleLibro(libro1)
+
+function prestarLibro(titulo, usuario) {
+    console.log("El libro "+titulo +" ha sido prestado a "+ usuario+".")
+}
+
+prestarLibro("1984", "Ana")
+
+function devolverLibro(titulo, diasRetraso = 0) {
+    const multa= diasRetraso * 0.5;
+    const mensaje = diasRetraso > 0
+        ? `Devuelto con ${diasRetraso} días de retraso. Multa: $${multa}`
+        : `Devuelto a tiempo. No hay multa`;
+    console.log(`Libro: ${titulo}: ${mensaje}`);
+}
+
+devolverLibro("El principito", 4);
+
+(function(){
+    let mensaje ="Ejecutada al instante.";
+    console.log(mensaje);
+
+})()
