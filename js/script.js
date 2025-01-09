9f7d9d8c43a9e449eeed8f2a3230e063e3442a38
LucasGuimil <l.d.guimil@gmail.com>
2025-01-09 20:19:18 -0300

    Modificación y busqueda dentro de arrays

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
-/* Declarar arrays  */
+// Metodos de modificación de arrays
 
+//Push: añade uno o más elementos al final de un array
+let frutas = ["Banana","Manzana","Cereza","Durazno"]
+console.log(frutas)
+frutas.push("Pera")
+console.log(frutas)
 
-let numeros = [1,2,3,4,5]
-console.log(numeros)
+//Pop: elimina el último elemento de un array, el mismo se puede guardar en otra variable.
+let frutaEliminada = frutas.pop()
+console.log(frutaEliminada)
+console.log(frutas)
 
-let palabras = ["manzana", "pera", "auto","progamacion"]
-console.log(palabras[3])
+//Shift: elimina el primer elemento del array y desplaza los demás elementos a una posición inferior.
+let primeraFruta = frutas.shift()
+console.log(primeraFruta)
+console.log(frutas)
 
-let booleanos = [true, false,true,false]
-console.log(booleanos)
+//Unshift: añade uno o más elementos al inicio del array, modificando los índices de los demás elementos.
+frutas.unshift("Pomelo","Kiwi")
+console.log(frutas)
 
-let matriz = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
+//Métodos de busqueda dentro de arrays.
 
-console.log(matriz)
-console.log(matriz[0][2])
+//indexOf: devuelve el primer índice en el que se puede encontrar un elemento dentro del array o -1 si no se encuentra. Es sensible a todo tipo de datos, diferencia entre números, strings, etc.
 
-/*Uso de for para recorrer un array */ 
+let nombres = ["Ana", "Juan","Carlos","Marta"]
+let indice=nombres.indexOf("Carlos")
+console.log(indice)
 
-let numbers = [10,20,30,40,50]
-for (let i=0;i<numbers.length;i++) {
-    console.log(numbers[i])
-}
+/*Casos de uso: 
+Validación de entrada: Verificar si ya hay un elemento antes de agregarlo a un array.
+Localización de elementos para su manipulación: Determinar la posición para actualizar o eliminar un elemento.*/
 
-const frutas = ["Manzana","Banana","Cereza"]
-console.log(frutas[0])
-console.log(frutas[1])
-console.log(frutas[frutas.length-1])
+//includes: determina si el array contiene un determinado elemento, devolviendo un valor true o false. No proporciona la ubicación del elemento, sólo verifica si existe.
+
+let autos = ["Fiat","Ford","Toyota"]
+let contiene = autos.includes("Nissan")
+console.log(contiene)
+
+/*Casos de uso: 
+Condiciones: ejecutar código basado en la presencia de un elemento de un array.
+Filtrado de opciones: Verificar rapidamente la inclusión de elementos sin preocuparse por su ubicación exacta*/
+
