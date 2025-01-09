b09dc5345c999092238291a982522febb34d56e5
LucasGuimil <l.d.guimil@gmail.com>
2025-01-09 19:18:10 -0300

    Arreglando parseInt y muestra de resultado

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // ACtividad de armar 3 funciones
 
 function ingresarNumeros() {
-    let num = prompt("Ingrese un número")
-    return Number(num)
+    let num = parseInt(prompt("Ingrese un número"))
+    return num
 }
 
 const sumarNumeros = (a, b) => a + b;
 
 function mostrarResultado () {
     const resultado = sumarNumeros(ingresarNumeros(),ingresarNumeros());
-    console.log(`El resultado de la suma es: ${resultado}.`);
+    alert(`El resultado de la suma es: ${resultado}.`);
 }
 
 document.getElementById("boton").addEventListener('click', ()=> {
