cbdfc2618737c8c616d30dd10ed2e5a7a56041d6
LucasGuimil <l.d.guimil@gmail.com>
2025-01-09 20:33:49 -0300

    Ordenando arrays con sort y reverse

--- js/script.js
+++ js/script.js
@@ -1,44 +1,17 @@
-// Metodos de modificación de arrays
+//Ordenar arrays
 
-//Push: añade uno o más elementos al final de un array
-let frutas = ["Banana","Manzana","Cereza","Durazno"]
-console.log(frutas)
-frutas.push("Pera")
-console.log(frutas)
+//Método sort(): sirve para ordenar un array desordenado de manera creciente o de manera decreciente. Cuando no se determina ninguna función dentro del paréntesis, toma el sistema Unicode que ordena en base al primer caractér.
 
-//Pop: elimina el último elemento de un array, el mismo se puede guardar en otra variable.
-let frutaEliminada = frutas.pop()
-console.log(frutaEliminada)
-console.log(frutas)
+let numeros = [40,5,200,30,9]
+numeros.sort((a,b)=> a - b) /*Esta función flecha compara el primer parámetro con el segundo, ordenandolo de manera creciente. Si se pone al revés, ordena de manera decreciente.*/
+console.log(numeros)
 
-//Shift: elimina el primer elemento del array y desplaza los demás elementos a una posición inferior.
-let primeraFruta = frutas.shift()
-console.log(primeraFruta)
-console.log(frutas)
+let palabras = ["Auto","zorro","Pelota","casa"]
+/*Si no se define una función en el paréntesis, las palabras en mayúsculas siempre quedaran primeras.*/
+palabras.sort((a,b)=> a.localeCompare(b))//La función localeCompare realiza la comparación del orden alfábetico, ignorando las mayúsculas y minúsculas.
+console.log(palabras)
 
-//Unshift: añade uno o más elementos al inicio del array, modificando los índices de los demás elementos.
-frutas.unshift("Pomelo","Kiwi")
-console.log(frutas)
+//Método reverse(): invierte el orden del array.
 
-//Métodos de busqueda dentro de arrays.
-
-//indexOf: devuelve el primer índice en el que se puede encontrar un elemento dentro del array o -1 si no se encuentra. Es sensible a todo tipo de datos, diferencia entre números, strings, etc.
-
-let nombres = ["Ana", "Juan","Carlos","Marta"]
-let indice=nombres.indexOf("Carlos")
-console.log(indice)
-
-/*Casos de uso: 
-Validación de entrada: Verificar si ya hay un elemento antes de agregarlo a un array.
-Localización de elementos para su manipulación: Determinar la posición para actualizar o eliminar un elemento.*/
-
-//includes: determina si el array contiene un determinado elemento, devolviendo un valor true o false. No proporciona la ubicación del elemento, sólo verifica si existe.
-
-let autos = ["Fiat","Ford","Toyota"]
-let contiene = autos.includes("Nissan")
-console.log(contiene)
-
-/*Casos de uso: 
-Condiciones: ejecutar código basado en la presencia de un elemento de un array.
-Filtrado de opciones: Verificar rapidamente la inclusión de elementos sin preocuparse por su ubicación exacta*/
-
+numeros.reverse()
+console.log(numeros)
